9771e05cf603f258df1e013e584c70f84b394f8f
lumetrymedia <lumetrymedia@gmail.com>
2025-01-14 00:58:02 +0530

    Update app.js

--- app.js
+++ app.js
@@ -112,7 +112,7 @@ app.get("/events", async (req, res) => {
 
 
 app.post("/update-event", async (req, res) => {
-  const { eventID, eventName, eventDate, promptTitle, prompt, negativePrompt, event_logo, logo_placement, promptsList } = req.body;
+  const { eventID, eventName, eventDate, promptTitle, prompt, negativePrompt, event_logo, branding_logo, logo_placement, promptsList } = req.body;
 
   try {
     const event = await Event.findOneAndUpdate(
@@ -124,6 +124,7 @@ app.post("/update-event", async (req, res) => {
         prompt: prompt,
         negative_prompt: negativePrompt,
         event_logo: event_logo,
+        branding_logo: branding_logo,
         logo_placement: logo_placement,
         promptsList: promptsList
       },
